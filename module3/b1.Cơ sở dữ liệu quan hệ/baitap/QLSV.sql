f4036c5dd2e53106381d58124577a660fad272de
tranquocnha <tranquocnha180800@gmail.com>
2021-10-01 20:24:41 +0700

    nha nop bai tap

---
+++ module3/b1.Cơ sở dữ liệu quan hệ/baitap/QLSV.sql
@@ -0,0 +1,12 @@
+create  table student_management.Class (
+id int not null,
+name varchar(45) null,
+primary key(id));
+
+create  table student_management.Teacher (
+id int not null,
+name varchar(45) null,
+age int null,
+contry varchar(45) null,
+primary key(id)
+ );
---
+++ module3/b1.Cơ sở dữ liệu quan hệ/thuchanh/newtable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `student-management`.`student`(
+ `id` INT NOT NULL,
+ `name` VARCHAR(45) NULL,
+ `age` INT NULL,
+ `country` VARCHAR(45) NULL,
+PRIMARY KEY(`id`));
