ee5c7bf394f679637a37d3dd43f134924d715dab
tranquocnha <tranquocnha180800@gmail.com>
2021-12-05 14:33:06 +0700

    nha nop bai tap ngay 4/12

---
+++ module4/b5_thao_tac_voi_csdl_va_orm/bai_tap/b2_buc_anh_cua_ngay/src/main/java/codegym/repository/CommenRepositoryImpl.java
@@ -0,0 +1,26 @@
+package codegym.repository;
+
+import codegym.model.Commen;
+import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class CommenRepositoryImpl implements CommenRepository{
+    @Override
+    public List<Commen> findAll() {
+        Session session = null;
+        List<Commen> commentList = new ArrayList<>();
+        try {
+            session = ConnectionUtil.sessionFactory.openSession();
+            commentList = session.createQuery("FROM Commen").getResultList();
+        } finally {
+            if (session != null)
+                session.close();
+        }
+        return commentList;
+    }
+
+}
